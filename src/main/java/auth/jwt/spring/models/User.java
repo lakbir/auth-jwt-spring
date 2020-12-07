a27cfc5392dd38cb08ea4c8c7c1b06fe49c53ae2
Abderrahim Lakbir <lakbir.abderrahim@edigitalis.fr>
2020-12-07 22:18:07 +0100

    first commit

--- src/main/java/auth/jwt/spring/models/User.java
+++ src/main/java/auth/jwt/spring/models/User.java
@@ -1,16 +1,21 @@
 package auth.jwt.spring.models;
 
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(	name = "users", 
